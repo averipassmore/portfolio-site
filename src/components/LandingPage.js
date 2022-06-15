07372343a6514c885ca34828b1b920fee4f61b81
averinpassmore <averinpassmore@gmail.com>
2022-06-14 22:19:46 -0700

    finished background and nav button animation

---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import anpLogo from "../images/anpLogo.png"
+
+const LandingPage = () => {
+  return (
+    <div className="Twinkle-wrapper">
+      <img src={anpLogo} alt="" className="Nav-button"></img>
+    </div>
+  )
+}
+
+export default LandingPage;
