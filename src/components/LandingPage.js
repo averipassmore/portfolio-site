1ffc0259a02f5ca527726d811d4a5543ee577685
averinpassmore <averinpassmore@gmail.com>
2022-06-15 15:01:04 -0700

    finished top section

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,27 +1,31 @@
 import React from "react";
 import anpLogo from "../images/anpLogo.png"
 import warp from '../images/warp.gif'
+import { Link } from "react-router-dom";
 
 const LandingPage = () => {
   return (
     <div>
-      <div className="Twinkle-wrapper">
+      <div className="Twinkle-wrapper"></div>
       <div className="Dropdown">
-        <img src={anpLogo} alt="" className="Dropdown-button"></img>
-        <div className="Dropdown-content">
-          <p>About Me</p>
-          <p>Projects</p>
-          <p>Contact</p>
+          <img src={anpLogo} alt="" className="Dropdown-button"></img>
+          <div className="Dropdown-content">
+            <Link to="About" style={{"text-decoration": "none"}}><p>About Me</p></Link>
+            <Link to="Projects" style={{"text-decoration": "none"}}><p>Projects</p></Link>
+            <Link to="Contact" style={{"text-decoration": "none"}}><p>Contact</p></Link>
+          </div>
+        </div>
+
+      <div className="Secret-wrapper">
+        <p>psst</p>
+        <p id="hidden">looking for some fun?</p>
+        <div className="Warp-cropper">
+          <Link to="/secret"><img src={warp} class="Warp" alt=""></img></Link>
         </div>
       </div>
-    </div>
-    <div className="Secret-wrapper">
-      <p>psst</p>
-      <p id="hidden">looking for some fun?</p>
-      <div className="Warp-cropper">
-        <img src={warp} class="Warp" alt=""></img>
+      <div className="Top-title">
+        <h1>welcome to my safe space</h1>
       </div>
-    </div>
     </div>
   )
 }
