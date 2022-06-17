e405bb26d33540286805f76958ef46273fdf81d5
averinpassmore <averinpassmore@gmail.com>
2022-06-17 12:36:46 -0700

    integrated emailjs functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,6 @@
 import React, {useRef} from "react";
 import { useNavigate } from "react-router-dom";
-// import emailjs from 'emailjs-com';
+import emailjs from 'emailjs-com';
 
 const Contact = () => {
   const form = useRef();
@@ -38,16 +38,22 @@ const Contact = () => {
     if(!window.confirm("Please confirm request")) return;
 
     // works just dont want to use all of the emails
-    // emailjs.sendForm('service_vxo5ko4', 'template_bnbzl4f', form.current, 'Um-6jVm3sb72fybJ3')
-    //   .then((result) => {
-    //       console.log(result.text);
-    //       alert('Your request was successfully submitted');
-    //   }, (error) => {
-    //       console.log(error.text);
-    //       alert("Your request failed... Please try again", error)
-    //       return;
-    //   });
-    window.location.reload();
+    emailjs.sendForm('service_zid19jo', 'template_54s1gsj', form.current, 'Um-6jVm3sb72fybJ3')
+      .then((result) => {
+          console.log(result.text);
+          const name = document.getElementById('name');
+          const email = document.getElementById('email');
+          const message = document.getElementById('message');
+          name.value = "";
+          email.value = "";
+          message.value = "";
+          alert('Your request was successfully submitted');
+      }, (error) => {
+          console.log(error.text);
+          alert("Your request failed... Please try again", error)
+          return;
+      });
+    // window.location.reload();
   };
 
   return (
@@ -56,12 +62,12 @@ const Contact = () => {
         <h1>Contact</h1>
         <form className="Contact-form"  ref={form} onSubmit={sendEmail}>
           <label htmlFor="user_name"></label>
-          <input type="text" name="user_name" placeholder="Name"/>
+          <input type="text" name="user_name" placeholder="Name" id="name"/>
           <label></label>
-          <input type="text" name="user_email" placeholder="example@email.com"/>
+          <input type="text" name="user_email" placeholder="example@email.com" id="email"/>
           <label htmlFor="typeOfService"></label>
           <textarea name="message" placeholder="Business inquiry? Want to work together? Leave some details and
-          I will get back to you as soon as possible!"></textarea>
+          I will get back to you as soon as possible!" id="message"></textarea>
           <input type="submit" value="Send" />
           </form>
       </div>
